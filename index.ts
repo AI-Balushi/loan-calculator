0bde6bea25bd3032e0a76c39aaf172a836e04efc
Abdul Waheed <waheedjdk@hotmail.com>
2024-03-23 22:18:44 +0500

    Created Loan Calculator

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+
+async function loanCalculator() {
+  console.log("Loan Calculator");
+
+  const questions = [
+    { type: "input", name: "PV", message: "Enter the loan amount:" },
+    { type: "input", name: "PMT", message: "Enter the monthly payment:" },
+    {
+      type: "input",
+      name: "i",
+      message: "Enter the monthly interest rate (in decimal form):",
+    },
+    { type: "input", name: "n", message: "Enter the number of months:" },
+  ];
+
+  const answers = await inquirer.prompt(questions);
+
+  const PV = parseFloat(answers.PV);
+  const PMT = parseFloat(answers.PMT);
+  const i = parseFloat(answers.i);
+  const n = parseInt(answers.n);
+
+  const PVCalculated = calculateLoanAmount(PMT, i, n);
+
+  console.log(`The loan amount you can get is: ${PVCalculated.toFixed(2)}`);
+}
+
+function calculateLoanAmount(PMT: number, i: number, n: number): number {
+  return (PMT * (1 - Math.pow(1 + i, -n))) / i;
+}
+
+loanCalculator();
